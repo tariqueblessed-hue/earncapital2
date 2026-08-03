5b0014d077774f4244e0507f8c1cb7d0b2b2ae17
Tarique Blessed <YOUR_GITHUB_EMAIL>
2026-08-03 09:34:05 +0300

    Fix email type

--- app/withdrawals/page.tsx
+++ app/withdrawals/page.tsx
@@ -56,7 +56,7 @@ export default function WithdrawPage() {
   return;
 }
 
-setEmail(user.email);
+setEmail(user.email ?? "");
 
     const currentUser =
       localStorage.getItem("currentUser") || "";
